e99e8c9061dd5bfbb76d9c9731e40da8bdcbec03
Vasia <Vasia333331@rambler.ru>
2023-03-12 22:02:11 +0300

    ДЗ3

---
+++ LS3/LS3_3.py
@@ -0,0 +1,9 @@
+def my_func(arg1 , arg2, arg3):
+    if arg1 >= arg3 and arg2 >= arg3:
+        return arg1 + arg2
+    elif arg1 > arg2 and arg1 < arg3:
+        return arg1 + arg3
+    else:
+        return arg2 + arg3
+
+print(f'Результат - {my_func(int(input("введи первый аргумент ")), int(input("введи второй аргумент ")), int(input("введи третий аргумент ")))}')
